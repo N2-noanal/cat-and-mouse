7b2c5d687b33ed3a58f503553cd2d3374153e0b4
noa <c0b2213790@edu.teu.ac.jp>
2024-11-08 02:07:18 +0900

    視界関数追加

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ class Cat:  #ネコ(プレイヤー)の管理
                 print("無効な入力です")
                     
 
-class Item:  #アイテム3種の管理
+class Item:  # アイテム3種の管理
     def __init__(self, cat_items, position=None):
         available_items = [item for item in ITEMS if item not in cat_items]
         if not available_items:
@@ -135,49 +135,48 @@ class Item:  #アイテム3種の管理
 
 
 def make_board():
-    return [["#" if i==0 or j == 0 or i == BOARD_HEIGHT-1 or j == BOARD_WIDTH-1 else '.' for j in range(BOARD_WIDTH)] for i in range(BOARD_HEIGHT)]
+    return [["#" if i == 0 or j == 0 or i == BOARD_HEIGHT-1 or j == BOARD_WIDTH-1 else '.' for j in range(BOARD_WIDTH)] for i in range(BOARD_HEIGHT)]
 
-def print_board(board, cat_position, mouse_position, item, loopholes):
-    loophole_positions = set(loopholes.keys())
+def update_vision_board(game_board, vision_board, cat_position, mouse_position, item_position, active_player, loopholes):
+    if active_player == 'cat':
+        vision_range = 1  # ネコの視界範囲を1マスとする
+        vision_center = cat_position  # ネコが中心の視界を更新
+    else:
+        vision_range = 2  # ネズミの視界範囲を2マスとする
+        vision_center = mouse_position  # ネズミが中心の視界を更新
+
+    # 全体のマップを'X'で初期化
     for i in range(BOARD_HEIGHT):
         for j in range(BOARD_WIDTH):
-            char = board[i][j]
-            if [i, j] == cat_position:
-                char = 'C'
-            elif [i, j] == mouse_position:
-                char = 'M'
-            elif [i, j] == item.position:
-                char = item.type
-            for loophole in loophole_positions:
-                hole = list(loophole)
-                if [i, j] == hole:
-                    char = 'h'
-            print(char, end=' ')
-        print()
-    print()
-    
-"""    
-def blind(character,borad):
-    if board[y][x] == HIDDEN:
-        board[y][x] = 
-    if character == "cat":
-        cat_mask = [(-1, -1), (-1, 0), (-1,1),
-        (0,-1), (0,0), (0,1),
-        (1,-1), (1,0), (1,1)]
+            vision_board[i][j] = "X"
 
-    else:
-        mouse_mask = [(-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2),
-        (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2),
-        (0, -2), (0, -1), (0, 0), (0, 1), (0, 2,),
-        (1, -2), (1, -1), (1, 0), (1, 1), (1,2),
-        (2, -2), (2, -1), (2, 0), (2, 1), (2, 2)]
-"""
+    # 視界範囲内のみ更新
+    for i in range(max(0, vision_center[0] - vision_range), min(BOARD_HEIGHT, vision_center[0] + vision_range + 1)):
+        for j in range(max(0, vision_center[1] - vision_range), min(BOARD_WIDTH, vision_center[1] + vision_range + 1)):
+            vision_board[i][j] = game_board[i][j]
+            if [i, j] == item_position:
+                vision_board[i][j] = 'I'  # アイテム表示
+            for loophole in loopholes:
+                if [i, j] == list(loophole):
+                    vision_board[i][j] = 'h'  # 抜け穴表示
+
+    # アクティブプレイヤーの位置を表示
+    vision_board[vision_center[0]][vision_center[1]] = 'C' if active_player == 'cat' else 'M'
+
+    return vision_board
+
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+    print()
 
 def check_win(cat_position, mouse_position):
     return cat_position == mouse_position
 
 def game_loop():
-    board = make_board()
+    game_board = make_board()  # ゲーム全体の状態を管理するマップ
+    vision_board = make_board()  # プレイヤーに表示されるマップ
     mouse = Mouse()
     cat = Cat()
     item = Item(cat_items)
@@ -186,65 +185,60 @@ def game_loop():
     while turn_count < 31:
         if turn_count % 2 == 1:
             # ネズミのターン
-            print(f'ターン{turn_count//2+1}')
-            # blind("mouse")
-            print_board(board, cat.position, mouse.position, item, mouse.loopholes)
+            active_player = 'mouse'
+        else:
+            # ネコのターン
+            active_player = 'cat'
+        
+        # ネコ、ネズミ、アイテムの位置を更新した表示マップを作成
+        update_vision_board(game_board, vision_board, cat.position, mouse.position, item.position, active_player, mouse.loopholes)
+        print_board(vision_board)  # 表示マップを出力
+        
+        if turn_count % 2 == 1:
             mouse_move = input("ネズミの移動 (WASD): ").upper()
             mouse.mouse_move(mouse_move)
             if check_win(cat.position, mouse.position):
                 print("勝利！ネズミが突っ込んできた！")
                 break
-            turn_count += 1
         else:
-            # ネコのターン
-            #blind("cat")
-            print_board(board, cat.position, mouse.position, item, mouse.loopholes)
-            use_item = input("アイテムを使用しますか？(Y:使用, 他:使わない)").upper
-            if use_item == "Y" and cat.use_item():
-                pass
             cat_move = input("ネコの移動 (QWEADZXC): ").upper()
             cat.next_move(cat_move)
             if check_win(cat.position, mouse.position):
                 print("勝利！ネズミをつかまえた！")
                 break
-            if cat.position == item.position:
-                print(f'{item.type}をゲット！')
-                cat.items.append(item.type)
-                cat_items.append(item.type)
-                item.position = [None, None]
-            turn_count += 1
-            
+
+        turn_count += 1
     else:
         print("ネズミに逃げられてしまった...")
-        
             
 def print_rule():
         print("=== ルール ===")
-        #ターンについて
-        print("#ターンについて")
+        # ターンについて
+        print("～ ターンについて ～")
         print("15ターン以内にネズミを捕まえたらネコの勝利です！")
         print("1ターンに1マス移動が必要です。")
         print("")
-        #移動方法について
-        print("#移動方法について")
+        # 移動方法について
+        print("～ 移動方法について ～")
         print("ネコの移動キー:\n↖q ↑w e↗\n←a s d→\n↙z ↓x c↘")
         print("ネズミの移動範囲は上下左右の4方向です。")
         print("")
-        #アイテム使用について
-        print("#アイテム使用について")
-        print("アイテムは\nL=ライト,S=シューズ,B=バケツ の三種類\n始めに部屋のどこかに1つ落ちているほか、\nネズミが抜け穴を使用するとランダムで1つ、\n使用された抜け穴の位置に生成されます。")
-        print("アイテムの使用方法：\n     数字キーにて  1=ライト、2=シューズ、3=バケツ\n   を所持しているアイテムに応じて押してください。\nアイテム使用をキャンセルしたい場合は、数字キー 0 を押してください。")
+        # アイテム使用について
+        print("～ アイテム使用について ～")
+        print("アイテムは\nL=ライト,S=シューズ,B=バケツ の三種類\nゲーム開始時に部屋のどこかに1つ落ちているほか、\nネズミが抜け穴(マップ上の「h」)を使用するとランダムで1つ、\n使用された抜け穴の位置に生成されます。")
+        print("☆ アイテムの使用方法 ☆\n     数字キーにて  1=ライト、2=シューズ、3=バケツ\n   を所持しているアイテムに応じて押してください。\nアイテム使用をキャンセルしたい場合は、数字キー 0 を押してください。")
         print("")
-        #視界範囲について
-        print("#視界範囲について")
-        print("ネコの視界:\n      〇 〇 〇\n      〇 Ｎ 〇\n      〇 〇 〇\n      Nをネコとすると〇が見える範囲となります。")
+        # 視界範囲について
+        print("～ 視界範囲について ～")
+        print("ネコの視界:\n      〇 〇 〇\n      〇 Ｃ 〇\n      〇 〇 〇\n      Ｃをネコとすると〇が見える範囲となります。")
         print("")
-        print("ネズミの視界:\n      〇 〇 〇 〇 〇\n      〇 〇 〇 〇 〇\n      〇 〇 Ｎ 〇 〇\n      〇 〇 〇 〇 〇\n      〇 〇 〇 〇 〇\n      Nをネズミとすると〇が見える範囲となります。")
+        print("ネズミの視界:\n      〇 〇 〇 〇 〇\n      〇 〇 〇 〇 〇\n      〇 〇 Ｍ 〇 〇\n      〇 〇 〇 〇 〇\n      〇 〇 〇 〇 〇\n      Ｍをネズミとすると〇が見える範囲となります。")
         print("")
+        # ゲーム開始
+        print("それでは、ゲームスタート！")
 
 
 
 if __name__ == "__main__":  #起動
     print_rule()
     game_loop()
-    pass
